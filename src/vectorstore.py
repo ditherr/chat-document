baf3b75b8aed9704bc26d135e9fc711dcb0a9b87
ditherr <adityahh.rm@gmail.com>
2024-09-17 13:53:27 +0700

    FAISS added

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,11 +1,12 @@
 from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def config_retriever(documents):
     # Create embeddings and store in vectordb
     model_embed = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    vectordb = Chroma.from_documents(documents=documents, embedding=model_embed)
+    vectordb = FAISS.from_documents(documents=documents, embedding=model_embed)
     retriever = vectordb.as_retriever()
     
     return retriever
